bfec5fd839ab454a91beee93c24992dddb3c4089
Oladapo Falusi <oladapofalusi@gmail.com>
2025-03-10 22:24:22 +0000

    Launch date

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <a className="p-4 bg-primary flex justify-center text-xl font-semibold dark:bg-primary-dark text-white shadow-lg hover:text-accent dark:hover:text-accent-dark">
+      Lauching on 17/3/2025
+    </a>
+  )
 }

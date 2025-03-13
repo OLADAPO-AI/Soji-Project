f23fcd4b3566e33b536ab5534fcb38b1ae3c1887
josrph135 <oluwafemijosrph@gmail.com>
2025-03-13 14:17:35 +0100

    updated tagline in hero-section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,7 +5,13 @@ const Homepage = () => {
   return (
     <div className="bg-hero-bg bg-cover bg-no-repeat bg-center min-h-screen w-screen">
       <div className="h-screen bg-black/30 flex items-center relative text-text">
-        <div className="lg:ml-4 px-4 py-4 bg-neutral-600/30 lg:w-[50%] md:w-[50%] w-full mx-auto lg:mx-0 lg:h-[50%] h-[50%] justify-center absolute font-bold flex flex-col bottom-10">
+        <div className="lg:ml-4 px-4 pb-4 bg-neutral-600/30 lg:w-[50%] md:w-[50%] w-full mx-auto lg:mx-0 lg:h-[50%] h-[50%]  pt-12 absolute font-bold flex flex-col bottom-10">
+          <div className="flex flex-row tracking-wider pb-4 items-center gap-8">
+            <span className="h-5 w-5 bg-accent-dark rounded-full"></span>
+            <span className="uppercase text-base lg:text-base font-serif">
+              Your digital partner
+            </span>
+          </div>
           <h2 className="text-[32px] lg:text-[50px]">
             Smart Digital Solutions, Real Business Growth
           </h2>

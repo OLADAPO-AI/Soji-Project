35355cab3073fef0574baebd056892468d6e2782
josrph135 <oluwafemijosrph@gmail.com>
2025-03-19 21:02:59 +0100

    images optimized

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./component/navbar";
 import Homepage from "./pages/Homepage";
 import Services from "./pages/servicesPage";
-import DevelopmentPage from "./pages/developmentPage";
-import CareersPage from "./pages/careersPage";
-import Blogpage from "./pages/blogPage";
-import WhyjoroPage from "./pages/whyjoroPage";
+// import DevelopmentPage from "./pages/developmentPage";
+// import CareersPage from "./pages/careersPage";
+// import Blogpage from "./pages/blogPage";
+// import WhyjoroPage from "./pages/whyjoroPage";
 
 import Contact from "./pages/contactPage";
 import UnderDevelopment from "./pages/under-development";
@@ -19,10 +19,10 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/development" element={<DevelopmentPage />} />
+          {/* <Route path="/development" element={<DevelopmentPage />} />
           <Route path="/joroServices" element={<WhyjoroPage />} />
           <Route path="/blog" element={<Blogpage />} />
-          <Route path="/careers" element={<CareersPage />} />
+          <Route path="/careers" element={<CareersPage />} /> */}
           <Route path="/getintouch" element={<Contact />} />
           <Route path="*" element={<UnderDevelopment />} />
         </Routes>

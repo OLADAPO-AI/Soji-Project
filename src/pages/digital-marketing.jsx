3513e45eb3d8e7afd397e42296db7879cb867331
josrph135 <oluwafemijosrph@gmail.com>
2025-04-02 03:12:45 +0100

    digital page updated

--- src/pages/digital-marketing.jsx
+++ src/pages/digital-marketing.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import DigitalHeroSection from "../component/digitalMarketing/heroSection";
+import Footer from "../component/Footer";
 
 const DigitalMarketing = () => {
   return (
     <div className="min-h-screen bg-black text-text font-poppins">
       <DigitalHeroSection />
+      <Footer />
     </div>
   );
 };
